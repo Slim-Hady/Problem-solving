eb703c7bae8eb4af33b665b5767456712b195c29
Mohamed Abdelhady Mohamed Elsogher <155487881+Slim-Hady@users.noreply.github.com>
2026-05-30 14:26:34 +0300

    Create Missing Number.cpp

---
+++ CSES/Introductory Problems/Missing Number.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+#define MOD 1000000007
+#define endl '\n'
+#define debug(x) cerr << #x << " = " << x << endl;
+#define SlimHady ios::sync_with_stdio(false); cin.tie(0);
+ 
+ 
+ 
+void solve() {
+    ll n; cin >> n;
+    ll sum = 0;
+    for (int i = 0; i < n - 1; i++) {
+        ll a; cin >> a;
+        sum += a;
+    }
+    ll summation = (n * (n + 1)) / 2;
+    cout << summation - sum << endl;
+}
+ 
+ 
+int main() {
+    SlimHady
+ 
+    #ifndef ONLINE_JUDGE
+        freopen("in.txt", "r", stdin);
+        freopen("out.txt", "w", stdout);
+    #endif
+ 
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+ 
+    }
+    return 0;
+}
